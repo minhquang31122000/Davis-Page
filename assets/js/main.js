ab19a64790a70a03763c9892cf75b34988c91de5
MinhQuang3112 <89634314+MinhQuang3112@users.noreply.github.com>
2022-03-31 13:46:37 +0700

    update slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-
+ 
 loadingBg()
 collapsible()
 ripplesBg()
@@ -253,3 +253,23 @@ function checkForm(){
         }
     }
 }
+var pageHeight = 0;
+var viewpoint=0
+function findHighestNode(nodesList) {
+    for (var i = nodesList.length - 1; i >= 0; i--) {
+        if (nodesList[i].scrollHeight && nodesList[i].clientHeight) {
+            var elHeight = Math.max(nodesList[i].scrollHeight, nodesList[i].clientHeight);
+            pageHeight = Math.max(elHeight, pageHeight);
+        }
+        if (nodesList[i].childNodes.length) findHighestNode(nodesList[i].childNodes);
+    }
+    
+}
+
+findHighestNode(document.documentElement.childNodes);
+document.addEventListener('scroll', function(e){
+    console.log([e])
+     
+})
+// The entire page height is found
+console.log('Page height is', viewpoint);
